b9be424bb9f78a701d5d1d88dff1294b4fc3b376
Renzo Arias <core.editor>
2023-01-03 13:22:02 -0500

    Eliminacion de aboutme.js en raiz

--- aboutme.js
+++
@@ -1,6 +0,0 @@
-var miNombre = "Renzo Arias";
-var miProfesion = "Ingeniero de sonido";
-var yoEstare = "Web Development";
-
-//Usando temple literals
-var miExpresion = (`Hola me llamo ${miNombre} y soy ${miProfesion}. Estoy cursando este Master y me gustaria trabajar en ${yoEstare}`)
