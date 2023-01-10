3d23fbaee57569da24a6f7d02862f8618066bc3d
Renzo Arias <rariasbarrero@gmail.com>
2023-01-10 01:09:01 -0500

    Erjecicio 1

---
+++ activity/2_session/ejercicio_1.js
@@ -0,0 +1,10 @@
+var myBook = {
+    titulo : "El Médico",
+    autor : "Noah Gordon",
+    editorial : "Simon and Schuster",
+    año : 1986,
+    categoria : "novela historica",
+    paginas : 608
+};
+
+console.log(Object.values(myBook));
