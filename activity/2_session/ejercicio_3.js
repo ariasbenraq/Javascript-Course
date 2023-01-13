b338aff4788c47dbe94e7e221ec100134d51198c
Renzo Arias <rariasbarrero@gmail.com>
2023-01-12 23:31:21 -0500

    Ejercicio 3

---
+++ activity/2_session/ejercicio_3.js
@@ -0,0 +1,29 @@
+//Ejercicio resuelto con la ayuda de Javier, compañero del Master
+class InvertirCadena {
+    cadenaInvertir = "";
+
+    constructor(cadena){
+        this.cadenaInvertir = cadena;
+    }
+    myMethod() {
+        this.invert()
+    }    
+        invert = () => {
+            if (this.cadenaInvertir == "") {
+                try{
+                    throw ("No hay string aun XD");
+                }catch (e){
+                    console.log(e);
+                }
+            }
+            else {
+                //  return console.log(this.cadenaInvertir.split("").reverse("").join(""));
+            }
+        }    
+}
+
+var invertirCadena = new InvertirCadena("");
+
+const reverseString = str => str.split("").reverse().join("");
+console.log(reverseString(invertirCadena.cadenaInvertir));
+invertirCadena.invert();
