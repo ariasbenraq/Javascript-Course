bb80e9c1136af390a13ff580e806f44e7f41b8fb
Renzo Arias <rariasbarrero@gmail.com>
2023-01-13 16:29:20 -0500

    Actualización ejercicio 6 sesion 2

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -13,25 +13,25 @@ var loginFailure = document.getElementById("loginFailure");
 loginSuccess.addEventListener("click", function(){console.log("Correcto mano!")});
 loginFailure.addEventListener("click", function(){console.log("Incorrecto mano!")});
 // Ejercicio 6
-var loginSuccessAsync = document.getElementById("loginSuccessAsync");
-var loginFailureAsync = document.getElementById("loginFailureAsync");
+// var loginSuccessAsync = document.getElementById("loginSuccessAsync");
+// var loginFailureAsync = document.getElementById("loginFailureAsync");
 
 
 
-let loginWitUsername = (username, password) => {
-    return new Promise(function (resolve, rejected) {
-      setTimeout(() => {
-        if (username === "admin" && password === "passwd") {
-          resolve("User logged in");
-        } else {
-          rejected(new Error("Error: invalid username or password"));
-        }
-      }, 200);
+// let loginWitUsername = (username, password) => {
+//     return new Promise(function (resolve, rejected) {
+//       setTimeout(() => {
+//         if (username === "admin" && password === "passwd") {
+//           resolve("User logged in");
+//         } else {
+//           rejected(new Error("Error: invalid username or password"));
+//         }
+//       }, 200);
       
-    });
-};
+//     });
+// };
 
 
-loginSuccessAsync.addEventListener("click", function(){console.log("Todo bien mano!")});
-loginFailureAsync.addEventListener("click", loginWitUsername("admin", "passwd"));
+// loginSuccessAsync.addEventListener("click", function(){console.log("Todo bien mano!")});
+// loginFailureAsync.addEventListener("click", loginWitUsername("admin", "passwd"));
 
